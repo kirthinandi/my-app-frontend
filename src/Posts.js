ccd915c8c7323650a2ec5f0a9d5dbd2702f6d406
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-09-16 13:56:08 -0700

    added images & styled them

--- src/Posts.js
+++ src/Posts.js
@@ -16,6 +16,14 @@ export default function Posts() {
 
     return (
         <div>
+            <br></br>
+            <img className="image" src="https://c.tenor.com/QZEM4sbHtp8AAAAC/gilmore-girls-rory-gilmore.gif"></img>
+            <img className="image" src="https://media3.giphy.com/media/xUySTJ8Y5HsuM4fvSE/giphy.gif"></img>
+            <img className="image" src="https://media3.giphy.com/media/xUPOqAXVSedL6QUIdG/giphy.gif"></img>
+            <img className="image" src="https://thumbs.gfycat.com/MeatyGranularAegeancat-max-1mb.gif"></img>
+            <img className="image" src="https://i.gifer.com/Rgjb.gif"></img>
+            <img className="image" src="https://i.gifer.com/XhWI.gif"></img>
+            
             <PostForm />
             <PostList posts={posts}/>
         </div>
