59c1ed03c3f5802042265672794541dd64f38c2a
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-11-30 20:18:02 -0800

    CSS changes

--- src/PostForm.js
+++ src/PostForm.js
@@ -7,7 +7,9 @@ export default function PostForm({addPost}) {
    const [date, setDate] = useState("")
    const [entry, setEntry] = useState("")
    const [categoryId, setCategoryId] = useState("")
-const [categories, setCategories] = useState([])
+   const [categories, setCategories] = useState([])
+
+
    useEffect(() => {
     fetch("http://localhost:9292/categories")
     .then(response => response.json())
@@ -15,7 +17,6 @@ const [categories, setCategories] = useState([])
       setCategories(data)
     })
   }, [])
-
 
 
   function handleSubmit(e) {
@@ -32,7 +33,6 @@ const [categories, setCategories] = useState([])
       entry: entry,
       category_id: categoryId
     })
-  
   })
   .then((r) => r.json())
   .then((newPost) => addPost(newPost))
@@ -40,16 +40,16 @@ const [categories, setCategories] = useState([])
 
   return (
     <>
-    <div className= "form">Add A New Diary Entry Here!</div>
-    <form onSubmit={handleSubmit}>
-        <input className="input" onChange = {(e) => setTitle(e.target.value)} value={title} type="text" title="title" placeholder="Enter Post's Title" />
-        <input className="input" onChange = {(e) => setDate(e.target.value)} value={date} type="text" date="date" placeholder="Enter Post's Date" />
-        <input className="input" onChange = {(e) => setEntry(e.target.value)} value={entry} type="text" entry="entry" placeholder="Post Entry" />
+    <div className="form-title">Add A New Diary Entry Here!</div>
+    <form onSubmit={handleSubmit} className= "form">
+        <span className="span">Enter title:</span> <input className="input" onChange = {(e) => setTitle(e.target.value)} value={title} type="text" title="title" placeholder="Enter Post's Title" />
+         <span className="span">Enter date:</span> <input className="input" onChange = {(e) => setDate(e.target.value)} value={date} type="text" date="date" placeholder="Enter Post's Date" />
+        <span className="span">Enter entry:</span> <input className="input" onChange = {(e) => setEntry(e.target.value)} value={entry} type="text" entry="entry" placeholder="Post Entry" />
         <select onChange={(e)=> setCategoryId(e.target.value)} name="emotions" className="button">
-        <option disabled selected value> -- select an option -- </option>
-            {categories.map(c => {
-                return <option value={c.id}>{c.emotion}</option>
-            })}
+            <option disabled selected value> -- select an option -- </option>
+                {categories.map(c => {
+                    return <option value={c.id}>{c.emotion}</option>
+                })}
         </select>
         <button className="button" type="submit">Create Post</button>
     </form>
